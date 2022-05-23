2cd3e4947061eee0241a70fc6413e7ed5e0bbddf
SAI <poojithsai98@gmail.com>
2022-05-23 17:55:02 +0530

    first commit

---
+++ src/seleniumGrid/Google.java
@@ -0,0 +1,22 @@
+package seleniumGrid;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class Google {
+
+	@Test
+	public void check() throws MalformedURLException {
+		DesiredCapabilities cap=new DesiredCapabilities();
+		cap.setBrowserName("chrome");
+		WebDriver driver=new RemoteWebDriver(new URL("http://192.168.43.234:4444"),cap);
+		driver.get("http://google.com");
+		driver.findElement(By.name("q")).sendKeys("sai");
+	}
+}
